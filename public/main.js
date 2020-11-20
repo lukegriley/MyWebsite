b942344cb40289a3ff733303f7a951f22a4eb0b3
joondae <68300257+joondae@users.noreply.github.com>
2020-11-20 02:11:38 -0800

    implemented rainbow text toggle button
    implemented start/stop music toggle button

--- public/main.js
+++ public/main.js
@@ -2,10 +2,14 @@ $("#snakebutton").click(requestSnakeContent);
 $("#stickbutton").click(requestStickContent);
 $("#aboutbutton").click(requestAboutContent);
 $("#playgame").click(requestGameContent);
+$("#rainbow_button").click(rainbowText);
+$("#stop_button").click(stopMusic)
 
-var audio = document.getElementById("audioControls");
-audioControls.volume = 0.05;
-
+//COMMENTED THIS STUFF OUT BECAUSE IT DOESN'T APPEAR ANYWHERE ELSE IN main.js, index.html, OR main.css
+//var audio = document.getElementById("audioControls");
+//audioControls.volume = 0.05;
+var isRainbow = false;
+var isPlayingSong = true;
 
 function requestSnakeContent() {
   $.ajax({url: "snakegame.html", success: loadContent});
@@ -21,6 +25,11 @@ function requestGameContent() {
   $.ajax({url: "game.html", success: loadGameContent});
 }
 
+function requestRainbowContent() {
+  console.log("reached inside of requestRainbowContent function");
+  $.ajax({url: "index.html", success: rainbowText});
+}
+
 function loadContent(result) {
   unloadContent();
   $("#mainContainer").html(result);
@@ -28,6 +37,28 @@ function loadContent(result) {
 function loadGameContent(result) {
   unloadContent();
   $("#game_content").html(result);
+}
+
+function rainbowText() {
+  if(isRainbow == false) {
+    document.getElementById("firstHeading").style = "background-image: linear-gradient(to left, violet, indigo, blue, green, yellow, orange, red); -webkit-background-clip: text; color: transparent;";
+    isRainbow = true;
+  }
+  else if(isRainbow == true) {
+    document.getElementById("firstHeading").style.color = "white";
+    isRainbow = false;
+  }
+}
+
+function stopMusic() {
+  if(isPlayingSong == true) {
+    document.getElementById("audioSong").pause();
+    isPlayingSong = false;
+  }
+  else if(isPlayingSong == false) {
+    document.getElementById("audioSong").play();
+    isPlayingSong = true;
+  }
 }
 
 function unloadContent() {
